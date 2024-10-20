3ba267cff6639f5481ee44ce41a0fa4b816ca5ca
jskolnicki <33360411+jskolnicki@users.noreply.github.com>
2024-10-20 16:58:43 -0700

    add blinkstick color codes to represent script statuses

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from data_sources.oura.processing import update_oura_sleep_data
 from database.db_manager import DatabaseManager
 from database.models import get_database_engine
 from utils.logging_config import setup_logging
+from utils.blinkstick import indicate_status, turn_off, set_color
 
 # Set up logging
 logger = setup_logging()
@@ -21,6 +22,7 @@ start_date = end_date - timedelta(days=3)
 # start_date = date(2023, 1, 1)
 
 def main():
+    turn_off()  # Reset BlinkStick from previous run
     logger.debug(f"Processing data from {start_date} to {end_date}")
 
     engine = get_database_engine()
@@ -33,11 +35,15 @@ def main():
 
     for source_name, update_function in data_sources:
         try:
+            indicate_status("processing")
             update_function(db_manager, start_date, end_date)
+            indicate_status("success", persist=True)
         except Exception as e:
             # Errors are logged inside the individual functions
             # Print here for immediate visibility when running the script
             print(f"Error occurred in {source_name} update: {str(e)}")
+            indicate_status("error", persist=True)
+            set_color("green", brightness=20)
 
 if __name__ == "__main__":
     main()
